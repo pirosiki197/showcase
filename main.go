e622f3560e6db33f87747868e55e61703b4d7ecd
pirosiki197 <lingmukangtai825@gmail.com>
2023-07-17 00:19:46 +0900

    add db connection

--- main.go
+++ main.go
@@ -1,11 +1,31 @@
 package main
 
 import (
+	"log"
+	"os"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
+	conf := mysql.Config{
+		User:   os.Getenv("DB_USER"),
+		Passwd: os.Getenv("DB_PASSWORD"),
+		Net:    "tcp",
+		Addr:   os.Getenv("DB_HOSTNAME") + os.Getenv("DB_PORT"),
+		DBName: os.Getenv("DB_DATABASE"),
+	}
+	db, err := sqlx.Open("mysql", conf.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
 	e := echo.New()
 	e.Use(middleware.Logger())
 
