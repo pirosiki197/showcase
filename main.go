ef5aadd05164b5265ca81784684b785e46eeab00
pirosiki197 <lingmukangtai825@gmail.com>
2025-05-18 13:41:52 +0900

    json

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"os/signal"
@@ -33,6 +35,24 @@ func main() {
 	})
 	e.GET("/env", func(c echo.Context) error {
 		return c.String(200, os.Getenv("EXAMPLE_ENV"))
+	})
+	e.GET("/json", func(c echo.Context) error {
+		data := map[string]any{
+			"message": "Hello, JSON!",
+			"nested": map[string]any{
+				"key":   "value",
+				"array": []any{"item1", "item2"},
+				"int":   123,
+				"bool":  true,
+			},
+		}
+		encoded, err := json.Marshal(data)
+		if err != nil {
+			log.Println("Error encoding JSON:", err)
+			return c.String(http.StatusInternalServerError, "Internal Server Error")
+		}
+		log.Println(string(encoded))
+		return c.JSON(http.StatusOK, data)
 	})
 
 	go func() {
