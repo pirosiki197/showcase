37d1d7acf9fdf9a2a48579ba64a5a9924996c86d
pirosiki197 <lingmukangtai825@gmail.com>
2026-07-05 22:53:05 +0900

    ok

--- main.go
+++ main.go
@@ -21,11 +21,12 @@ import (
 
 func main() {
 	conf := mysql.Config{
-		User:   os.Getenv("DB_USER"),
-		Passwd: os.Getenv("DB_PASSWORD"),
-		Net:    "tcp",
-		Addr:   net.JoinHostPort(os.Getenv("DB_HOST"), os.Getenv("DB_PORT")),
-		DBName: os.Getenv("DB_NAME"),
+		User:                 os.Getenv("DB_USER"),
+		Passwd:               os.Getenv("DB_PASSWORD"),
+		Net:                  "tcp",
+		Addr:                 net.JoinHostPort(os.Getenv("DB_HOST"), os.Getenv("DB_PORT")),
+		DBName:               os.Getenv("DB_NAME"),
+		AllowNativePasswords: true,
 	}
 	db, err := sql.Open("mysql", conf.FormatDSN())
 	if err != nil {
